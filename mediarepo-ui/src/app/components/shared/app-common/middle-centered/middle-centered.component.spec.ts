218f3bffb2572fbf199a8adc4ade5fc3408b1ad6
trivernis <trivernis@protonmail.com>
2022-02-11 18:12:10 +0100

    Fix eslint issues
    Signed-off-by: trivernis <trivernis@protonmail.com>

--- mediarepo-ui/src/app/components/shared/app-common/middle-centered/middle-centered.component.spec.ts
+++ mediarepo-ui/src/app/components/shared/app-common/middle-centered/middle-centered.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 
-import { MiddleCenteredComponent } from './middle-centered.component';
+import {MiddleCenteredComponent} from "./middle-centered.component";
 
-describe('MiddleCenteredComponent', () => {
-  let component: MiddleCenteredComponent;
-  let fixture: ComponentFixture<MiddleCenteredComponent>;
+describe("MiddleCenteredComponent", () => {
+    let component: MiddleCenteredComponent;
+    let fixture: ComponentFixture<MiddleCenteredComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MiddleCenteredComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [MiddleCenteredComponent]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MiddleCenteredComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MiddleCenteredComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
--- mediarepo-ui/src/app/components/shared/app-common/middle-centered/middle-centered.component.ts
+++ mediarepo-ui/src/app/components/shared/app-common/middle-centered/middle-centered.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from "@angular/core";
 
 @Component({
-  selector: 'app-middle-centered',
-  templateUrl: './middle-centered.component.html',
-  styleUrls: ['./middle-centered.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: "app-middle-centered",
+    templateUrl: "./middle-centered.component.html",
+    styleUrls: ["./middle-centered.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MiddleCenteredComponent implements OnInit {
+export class MiddleCenteredComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    constructor() {
+    }
 }
